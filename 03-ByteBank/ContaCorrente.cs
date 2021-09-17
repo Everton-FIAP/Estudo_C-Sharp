43cc6eebbc60537a9c240d2212b857f4cdea468d
Everton-FIAP <rm88475@fiap.com.br>
2021-09-17 14:08:25 -0300

    Classe Conta Corrente criada

---
+++ 03-ByteBank/ContaCorrente.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _03_ByteBank
+{
+    class ContaCorrente
+    {
+        private string titular;
+        private int agencia;
+        private int numero;
+        private double saldo;
+
+        public ContaCorrente() { }
+        public ContaCorrente(string titular, int agencia, int numero, double saldo)
+        {
+            this.titular = titular;
+            this.agencia = agencia;
+            this.numero = numero;
+            this.saldo = saldo;
+        }
+
+        public void Sacar(double valor)
+        {
+            if(this.saldo > valor)
+            {
+                this.saldo -= valor;
+            }
+        }
+
+        public void Depositar(double valor)
+        {
+            if(valor > 0)
+            {
+                this.saldo += valor;
+            }
+        }
+
+        public void Transferir(ContaCorrente conta, double valor)
+        {
+            if(valor < this.saldo)
+            {
+                this.saldo -= valor;
+                conta.saldo += valor; 
+            }
+        }
+
+
+
+        public string ToString()
+        {
+            return "titular=" + this.titular + ", agencia=" + this.agencia + ", numero=" + this.numero + ", saldo= " + this.saldo;
+        }
+    }
+}
---
+++ 03-ByteBank/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace _03_ByteBank
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ContaCorrente contaDaGabriela = new ContaCorrente("Gabriela",0001,2382219,0.0);
+            ContaCorrente contaDaDaniela = new ContaCorrente("Daniela", 0001, 2382218, 100.0);
+
+            contaDaGabriela.Depositar(100);
+            contaDaGabriela.Sacar(50);
+            contaDaDaniela.Transferir(contaDaGabriela, 50);
+
+            Console.WriteLine(contaDaGabriela.ToString());
+            Console.WriteLine(contaDaDaniela.ToString());
+
+           
+
+            Console.ReadLine();
+        }
+
+    }
+}
